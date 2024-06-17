2df863efd286e62df1239b283f31b6f993720c5d
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-06-17 10:06:17 +0300

    With the fact that you can add an element to HTML from js everything is amazing

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+let maina = document.getElementById("main");
+maina.remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Johnson is the champion";
